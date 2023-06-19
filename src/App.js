ed5419c09f76805c292541f2ca4d377be552b130
Oladeebo marvlous <50726938+guitarforte@users.noreply.github.com>
2023-06-19 13:16:17 +0100

    imags

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import {   Routes, Route } from "react-router-dom"
 import { Header } from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Footer }  from "./components/Footer";
-import { Create } from "./components/Create/Create"
+import { Create } from "./components/Create/Create";
+import { Login } from "./pages/Login/Login"
+import { Account } from "./pages/Account/account";
+
 
 const App = () => {
   return (
@@ -14,6 +17,8 @@ const App = () => {
         
         <Routes>
         <Route path="/" element={ <Home/> } />
+        <Route  path='/login' element={<Login/>} />
+        <Route  path='/account' element={<Account/>} />
         <Route path="/create" element={ <Create/> } />
         </Routes>
        <Footer/> 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-import "./login.css"
-import back from "../../images/my-account.jpg"
+import "./Login.css"
+import back from "../../images/my-account.jpg";
+import  {Link} from "react-router-dom"
 
 export const Login = () => {
   return (
@@ -15,13 +16,16 @@ export const Login = () => {
             </div>
           </div>
 
-          <form>
-            <span>Username or email address *</span>
-            <input type='text' required />
-            <span>Password *</span>
-            <input type='password' required />
-            <button className='button'>Log in</button>
-          </form>
+    <main>
+        <div class="top-img"></div>
+        <form class="col-lg-5 col-md-7 col-sm-8 col-10 shadow bg-white p-5 text-center">
+            <h1 class="mb-5">Login</h1>
+            <input type="email" class="form-control my-4 rounded-0 p-0" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email'" />
+            <input type="password" class="form-control my-4 rounded-0 p-0" placeholder="Enter Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email'" />
+            <button class="btn text-white text-uppercase rounded-0 mt-5 mb-4 btn-block">Login</button>
+            <Link href="#" class="f-pass">Forgot Password?</Link>
+        </form>
+    </main>
         </div>
       </section>
     </>
