4810c098ab83272d38c141aed922aae48ea53c7b
Oladeebo marvlous <50726938+guitarforte@users.noreply.github.com>
2023-06-20 12:43:13 +0100

    name

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -33,8 +33,8 @@ export const User = () => {
                       <img src='https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg?auto=compress&cs=tinysrgb&w=600' alt='' />
                     </div>
                     <div className='text'>
-                      <h4>Eden Smith</h4>
-                      <label>Los Angeles, CA</label>
+                      <h4>Oladeebo marvelous</h4>
+                      <label>Ibadan, Nigeria</label>
                     </div>
                   </div>
                 </Link>
