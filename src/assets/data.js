1cb3d5f2027ff23340bccef48dd38aa27fc43f9e
Oladeebo marvlous <50726938+guitarforte@users.noreply.github.com>
2023-06-20 15:12:26 +0100

    POPULATED CONTENT

--- src/assets/data.js
+++ src/assets/data.js
@@ -35,13 +35,13 @@ export const nav = [
     {
       id: 1,
       category: "Life",
-      title: "Stay Calm And Surf",
+      title: "Stay Calm And Enjoy",
       src: picture1,
     },
     {
       id: 2,
       category: "Fashion",
-      title: "Becoming a Dragonfly",
+      title: "Becoming a Avater",
       src: "https://img.freepik.com/free-vector/handsome-man_1308-85984.jpg?size=626&ext=jpg&ga=GA1.1.1329957299.1665183790&semt=sph",
     },
     {
@@ -53,7 +53,7 @@ export const nav = [
     {
       id: 4,
       category: "Sport",
-      title: "Stay Calm And Surf",
+      title: "Recreation",
       src: "https://img.freepik.com/premium-photo/big-fire-burns-against-background-night-sky-close-up_185094-7188.jpg?size=626&ext=jpg",
     },
     {
@@ -65,30 +65,30 @@ export const nav = [
     {
       id: 6,
       category: "Health",
-      title: "Becoming a Dragonfly",
+      title: "Live fine",
       src: picture6,
     },
     {
       id: 7,
-      category: "Business",
+      category: "Music",
       title: "Stay Calm And Surf",
       src: picture7,
     },
     {
       id: 8,
       category: "Technology",
-      title: "There's always light at the end of the tunnel",
+      title: "New technology evolving",
       src: picture1,
     },
   ]
   export const blog = [
     {
       id: 1,
-      title: "POST NO.1 CONTAINING A YOUTUBE VIDEO",
+      title: "WELCOM TO THE BLOG WEBSITE",
       desc: "Quisque at tellus nec magna viverra euismod. Maecenas lobortis, nisl et sodales blandit, neque mi ullamcorper eros, id efficitur ex velit in ante. Suspendisse elementum congue lacus quis gravida. Nunc eu hendrerit risus. Quisque blandit, orci non maximus vulputate, ligula ante ornare enim, ac interdum lorem leo varius mauris. Aliquam non sem vel orci malesuada sagittis. Vivamus id ullamcorper tellus. In hac habitasse platea dictumst. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et sapien a arcu feugiat mattis. Suspendisse sed magna quis turpis rhoncus venenatis.",
-      category: "Nature",
+      category: "Music",
       cover: picture1,
-      date: "APRIL 05, 2018",
+      date: "APRIL 05, 2023",
     },
   
     {
@@ -97,15 +97,15 @@ export const nav = [
       desc: "Vestibulum suscipit nibh sed odio pharetra, condimentum lacinia nisi varius. Ut ac ornare diam. Suspendisse vitae quam risus. Nulla ullamcorper libero justo, vitae dictum est efficitur id. Aenean et lacinia arcu. Sed non dolor id risus dapibus congue. Quisque tincidunt vitae tellus ut malesuada. Nulla eleifend nibh a maximus consectetur. Duis et mi porta, bibendum leo non, fermentum enim. Vivamus sed diam suscipit, ultricies ipsum sed, efficitur dui. Sed pharetra purus at iaculis vehicula. Fusce pellentesque placerat ante, id elementum dolor maximus sed. Aenean viverra at lectus quis vehicula. Aenean at arcu odio. Etiam mauris justo, cursus finibus aliquam eget, fermentum nec nulla. Sed finibus viverra magna non sagittis. Mauris fringilla augue vel quam rhoncus, in tempor enim lacinia. Donec nibh velit, scelerisque id neque sit amet, ornare consectetur sem. Donec convallis nec justo id consequat. Donec sem leo, pulvinar sit amet nisi a, mollis gravida est. Duis porttitor eleifend cursus. Vivamus ac dapibus dolor. Nullam leo tellus, rhoncus sit amet tempor vel, malesuada vel augue. Morbi laoreet sem eget tortor pulvinar mattis. Maecenas commodo ultricies orci, ut blandit urna accumsan sit amet. Ut a porttitor orci, maximus feugiat turpis. In eu sodales orci, et volutpat felis. Quisque orci quam, gravida at nulla tempor, vulputate pretium magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent congue, magna vitae interdum blandit, arcu mauris placerat nulla, sit amet rhoncus justo risus in neque.",
       category: "Nature",
       cover: "https://img.freepik.com/free-vector/handsome-man_1308-85984.jpg?size=626&ext=jpg&ga=GA1.1.1329957299.1665183790&semt=sph" ,
-      date: "MARCH 15, 2018",
+      date: "MARCH 15, 2022",
     },
     {
       id: 3,
-      title: "START YOU CUP ON TABLE OF THAT",
+      title: "MUSIC, BEST MEDICINE FOR YOUR SOUL",
       desc: "Duis rutrum eu tortor et efficitur. Nulla ac consequat leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut vestibulum mattis est eu volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae tristique nibh. Quisque vel lectus interdum lacus ultricies venenatis. In ex nisl, commodo vitae risus ac, efficitur imperdiet dolor. Maecenas dignissim luctus mi sit amet mollis. Maecenas sit amet bibendum dolor, a molestie tellus. Donec et varius risus. Duis tincidunt mi lorem, vitae luctus turpis rutrum sit amet. Aliquam aliquet malesuada odio, ut pellentesque nisi tempus in. Ut fermentum, augue id accumsan aliquet, dolor dui euismod elit, nec fringilla mi justo nec mauris. Duis tellus nisl, elementum et urna at, molestie condimentum eros. Sed mattis quis risus id tincidunt. Donec ultrices vestibulum vestibulum.",
-      category: "Sports",
+      category: "MUSIC",
       cover: "https://img.freepik.com/premium-photo/big-fire-burns-against-background-night-sky-close-up_185094-7188.jpg?size=626&ext=jpg",
-      date: "MARCH 15, 2018",
+      date: "MARCH 15, 2014",
     },
     {
       id: 4,
@@ -117,7 +117,7 @@ export const nav = [
     },
     {
       id: 5,
-      title: "SMART TYPE OF GAMING WITH CO-OP",
+      title: "HAVE FUN DOING THE SPORT YOU LOVE BEST",
       desc: "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition.",
       category: "Sports",
       cover: picture6,
@@ -125,40 +125,40 @@ export const nav = [
     },
     {
       id: 6,
-      title: "TEACH YOUR DRAGON HOW TO FLY",
+      title: "TEACH YOUR DOG HOW TO FLY",
       desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
       category: "Fashion",
       cover: picture7,
-      date: "February 20, 2018",
+      date: "February 20, 2023",
     },
     {
       id: 7,
-      title: "TEACH YOUR DRAGON HOW TO FLY",
+      title: "CONSISTENCY IS THE KEY",
       desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
       category: "Fashion",
       cover: "https://img.freepik.com/free-photo/yoga-woman-young-woman-doing-yoga-morning_654080-1084.jpg?size=626&ext=jpg&ga=GA1.2.1329957299.1665183790&semt=sph",
-      date: "February 20, 2018",
+      date: "February 20, 2014",
     },
     {
       id: 8,
-      title: "TEACH YOUR DRAGON HOW TO FLY",
+      title: "BECOMING THE BEST VERSION OF YOU",
       desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
-      category: "Fashion",
+      category: "Tech",
       cover: "https://img.freepik.com/premium-photo/big-fire-burns-against-background-night-sky-close-up_185094-7188.jpg?size=626&ext=jpg",
-      date: "February 20, 2018",
+      date: "February 20, 2025",
     },
     {
       id: 9,
-      title: "TEACH YOUR DRAGON HOW TO FLY",
+      title: "HOW TO BECOME THE BEST OF YOURSELF",
       desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
       category: "Fashion",
       cover: picture5,
-      date: "February 20, 2018",
+      date: "February 10, 2023",
     },
   
     {
       id: 10,
-      title: "TEACH YOUR DRAGON HOW TO FLY",
+      title: "TEACH ME TO LOVE YOU",
       desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
       category: "Fashion",
       cover: picture5,
