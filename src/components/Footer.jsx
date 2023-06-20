03a289867bdf36116dee1b0c7416c656fd5643da
Oladeebo marvlous <50726938+guitarforte@users.noreply.github.com>
2023-06-20 13:59:13 +0100

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,25 +2,21 @@ import React from "react"
 import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai"
 import { BsFacebook } from "react-icons/bs"
 import { RiInstagramFill } from "react-icons/ri"
+import "./Footer.css"
 
 export const Footer = () => {
-    const style = {
-        padding : '0px 100px'
-    }
-    const font = {
-        fontSize : '20px'
-    }
+
   return (
     <>
       <footer className='boxItems'>
-        <div className='container-flex d-flex text-align-center' style={style}>
+        <div className='container-flex d-flex style'  >
         
           <p>Marve-forte - All right reserved</p>
           
-          <div style={font} className='social ms-auto'>
-            <BsFacebook className='icon me-4' />
-            <RiInstagramFill className='icon me-4' />
-            <AiFillTwitterCircle className='icon me-4' />
+          <div  className='social ms-auto '>
+            <BsFacebook className='icon me-1 me-md-1 me-sm-1 me-lg-4 me-xxl-4' />
+            <RiInstagramFill className='icon me-1 me-md-1 me-sm-1 me-lg-4 me-xxl-4' />
+            <AiFillTwitterCircle className='icon me-1 me-md-1 me-sm-1 me-lg-4 me-xxl-4' />
             <AiFillLinkedin className='icon' />
           </div>
         </div>
